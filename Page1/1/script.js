0dc741b4e1e1c37c61e008db9706ccafa6bba93d
Wasitpol Jongmanasjaroean <lnwkazeazelass@hotmail.co.th>
2019-10-22 13:31:04 +0700

    Update all

--- Page1/1/script.js
+++ Page1/1/script.js
@@ -5,18 +5,18 @@ function description(i){
     let db2 = document.getElementById("db2");
     if(i == 1){
         d1.style.display = "block";
-        db1.style.backgroundColor = "gray";
+        db1.style.backgroundColor = "#ffffe0";
         d2.style.display = "none";
         db2.style.backgroundColor = "#ffffe0";
-        db1.style.boxShadow = "3px 5px 15px 3px rgb(46, 46, 46) inset";
+        db1.style.boxShadow = "3px 5px 15px 3px #E8E9BD inset";
         db2.style.boxShadow = "";
     }
     else{
         d1.style.display = "none";
         db1.style.backgroundColor = "#ffffe0";
-        db2.style.boxShadow = "3px 5px 15px 3px rgb(46, 46, 46) inset";
+        db2.style.boxShadow = "3px 5px 15px 3px #E8E9BD inset";
         d2.style.display = "block";
-        db2.style.backgroundColor = "gray";
+        db2.style.backgroundColor = "#ffffe0";
         db1.style.boxShadow = "";
     }
 }
