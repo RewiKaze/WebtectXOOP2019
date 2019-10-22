6b315f0baa2af936d7fc89d5aecabc754c04accd
kittiwat <memoryeamkijkarn@gmail.com>
2019-10-22 11:26:36 +0700

    m

--- main/script.js
+++ main/script.js
@@ -4,21 +4,22 @@ function litpage() {
     let lp = document.getElementById("lit");
     let tiii = document.getElementById("titleii");
     
-    if(rp.className == "col-sm-5"){
-        rp.className = "ii";
-        rp.style.width = "1px";
-        trp.style.display = "none";
-        tiii.style.display = "none";
-        rp.style.left = "100vw";
-        lp.style.left = "60%";
+    if(rp.className == "ii"){
+        rp.className = "col-sm-5";
+       
+        trp.style.display = "grid";
+ 
+        tiii.style.display = "block";
+     
     }
     else{
-        lp.style.left = "0vw";
-        lp.style.right = "90vw";
-        rp.className = "col-sm-5";
-        rp.style.width = "40vw";
-        rp.style.left = "0vw";
-        trp.style.display = "grid"
-        tiii.style.display = "block";
+        rp.className = "ii";
+        rp.style.right = "0vw";
+        rp.style.left = "100vw";
+        trp.style.display = "none";
+        rp.style.width = "1px";
+        tiii.style.display = "none";
+        
     }
+   
 } 
