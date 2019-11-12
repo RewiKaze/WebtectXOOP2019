eb9d175b4320dd81f6cf06c0247a28244bdf37a1
Wasitpol Jongmanasjaroean <lnwkazeazelass@hotmail.co.th>
2019-11-12 15:15:48 +0700

    fix bug index

---
+++ main/checkUser.js
@@ -0,0 +1,27 @@
+function checkUser() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            document.getElementById("signupbox").innerHTML = "<div id='logoutbutton' style='cursor:pointer;' onclick='logout()'>Sign Out</div>";
+            document.getElementById("signinbox").innerHTML = "<div id='username'>" + user.displayName + "</div>";
+            document.getElementById("signinbox").style.display = "";
+            document.getElementById("signupbox").style.display = "";
+        } else {
+
+            document.getElementById("signinbox").innerHTML = "<div id='signinbutton' style='cursor:pointer;' onclick='window.location.href=\"../signin/signin.html\"'>Sign in</div>";
+            document.getElementById("signupbox").innetHTML = "<div id='signupbutton' style='cursor:pointer;' onclick='window.location.href=\"../signin/create1.html\"'>Sign up</div>";
+            document.getElementById("signinbox").style.display = "";
+            document.getElementById("signupbox").style.display = "";
+        }
+    });
+}
+function logout() {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+        window.location.reload();
+    }).catch(function (error) {
+        // An error happened.
+    });
+
+
+
+}
