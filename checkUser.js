cad27e0ed514e66ebbacfe4786e47392f904605e
kittiwat <memoryeamkijkarn@gmail.com>
2019-11-14 11:12:34 +0700

    index js link

--- checkUser.js
+++ checkUser.js
@@ -7,8 +7,8 @@ function checkUser() {
             document.getElementById("signupbox").style.display = "";
         } else {
 
-            document.getElementById("signinbox").innerHTML = "<div id='signinbutton' style='cursor:pointer;' onclick='window.location.href=\"../signin/signin.html\"'>Sign in</div>";
-            document.getElementById("signupbox").innetHTML = "<div id='signupbutton' style='cursor:pointer;' onclick='window.location.href=\"../signin/create1.html\"'>Sign up</div>";
+            document.getElementById("signinbox").innerHTML = "<div id='signinbutton' style='cursor:pointer;' onclick='window.location.href=\"signin/signin.html\"'>Sign in</div>";
+            document.getElementById("signupbox").innetHTML = "<div id='signupbutton' style='cursor:pointer;' onclick='window.location.href=\"signin/create1.html\"'>Sign up</div>";
             document.getElementById("signinbox").style.display = "";
             document.getElementById("signupbox").style.display = "";
         }
